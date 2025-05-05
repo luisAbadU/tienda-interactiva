bf206b06ad0f49fef9525466fd7ba25af8601b26
luisAbadU <labad@unal.edu.co>
2025-05-05 15:21:50 -0500

    feat: creacion de funciones JS

---
+++ funcion.js
@@ -0,0 +1,11 @@
+function init() {
+    var productos = document.getElementById("productos");
+    productos = productos.innerHTML =
+    `
+    <div>
+        <label>producto A</label>
+        <input type="number" name="" id="" min="1">
+        <button type="button" class="btn btn-primary">Agregar</button>
+    </div>`;
+
+}
